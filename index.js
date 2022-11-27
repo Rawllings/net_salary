28b911a013dfdc60c2334b512ec6573214d5dec8
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-27 16:35:56 +0300

    updating JS styles file

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function netSalary() {
     let salary = document.getElementById("salary").value;
     
-    // calculating the net salary of an employee earning below 24,000. 
+    // calculating the net salary of an employee earning below 0. 
 
-    if (salary < 24000){
+    if (salary < 0){
+    
+        document.getElementById("net").innerHTML = `Your net salary is 0`
+    }
+   
+     // calculating the net salary of an employee earning below 24,000.
+
+    else if (salary < 24000){
         let tax = salary * 0.1;
         let nssf = salary * 0.06;
         let nhif = salary * 0.025;
@@ -14,7 +21,7 @@ function netSalary() {
     }
 
      // calculating the net salary of an employee earning above 24,000 and below 32333. 
-     
+
     else if (salary >= 24000 && salary < 32333 ){
         let tax = salary * 0.25;
         let nssf = salary * 0.06;
