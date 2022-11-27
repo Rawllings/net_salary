68a83307d67b2e68db5bf80a281afb541d482863
Rawllings Kiprop <rutojr01@gmail.com>
2022-11-27 16:11:07 +0300

    Complete JS file to calculate net income

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 function netSalary() {
+    let salary = document.getElementById("salary").value;
+    
+    if (salary < 24000){
+        let tax = salary * 0.1;
+        let nssf = salary * 0.06;
+        let nhif = salary * 0.025;
+
+        let finSalary = salary - (tax + nssf + nhif);
+
+        document.getElementById("net").innerHTML = `Your net salary is ${finSalary}`
+    }
+
+    else if (salary >= 24000 && salary < 32333 ){
+        let tax = salary * 0.25;
+        let nssf = salary * 0.06;
+        let nhif = salary * 0.025;
+
+        let finSalary = salary - (tax + nssf + nhif);
+
+        document.getElementById("net").innerHTML = `Your net salary is ${finSalary}`
+    }
+
+    else if (salary > 32333){
+        let tax = salary * 0.3;
+        let nssf = salary * 0.06;
+        let nhif = salary * 0.025;
+
+        let finSalary = salary - (tax + nssf + nhif);
+
+        document.getElementById("net").innerHTML = `Your net salary is ${finSalary}`
+    }
+
+    else {
+        document.getElementById("net").innerHTML = `Enter salary`
+    }
+    
+    
     
 }
